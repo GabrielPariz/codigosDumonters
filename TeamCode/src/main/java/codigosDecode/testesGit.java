ddd5d3a1f79588e259541e9f85e84cbcd2328cb2
GabrielPariz <you@example.com>
2025-05-07 14:53:41 -0300

    "Nova Classe Teste"

--- TeamCode/src/main/java/codigosDecode/testesGit.java
+++ TeamCode/src/main/java/codigosDecode/testesGit.java
@@ -1,4 +1,12 @@
 package codigosDecode;
 
-public class testesGit {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class testesGit extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
