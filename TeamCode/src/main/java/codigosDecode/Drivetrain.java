3522dab929f9a53b10a6db7120722a212bbfc964
GabrielPariz <bielparizbarbosa@gmail.com>
2025-05-09 12:40:19 -0400

    "Classe Drivetrain Universal Criada"

---
+++ TeamCode/src/main/java/codigosDecode/Drivetrain.java
@@ -0,0 +1,23 @@
+package codigosDecode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Drivetrain extends LinearOpMode {
+
+    DcMotor motor_df;
+    DcMotor motor_ef;
+    DcMotor motor_dt;
+    DcMotor motor_et;
+
+    @Override
+    public void runOpMode() {
+
+        
+
+        waitForStart();
+        while(opModeIsActive()) {
+
+        }
+    }
+}
